10c297d9cee5485a8d5afc0d0e4eda7e75847703
PaladinKnightMaster <paladinknight.master@gmail.com>
2022-04-07 23:48:45 -0400

    finishing up deployment of contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-ethers");
-require("dotenv-safe").config();
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-ethers');
+require('hardhat-deploy');
+require('dotenv-safe').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 // You need to export an object to set up your config
@@ -20,21 +21,36 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.3",
-  networks: {
-    rinkeby: {
-      url: process.env.NEXTJS_APP_RINKEBY_RPC_URL,
-      accounts: [process.env.NEXTJS_APP_PRIVATE_KEY],
-      chainId: 4,
-    }
-  },
-  etherscan: {
-    apiKey: process.env.NEXTJS_APP_ETHERSCAN_KEY,
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  }
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.3',
+            },
+            {
+                version: '0.7.0',
+            },
+            {
+                version: '0.6.6',
+            },
+            {
+                version: '0.4.24',
+            },
+        ],
+    },
+    networks: {
+        rinkeby: {
+            url: process.env.NEXTJS_APP_RINKEBY_RPC_URL,
+            accounts: [process.env.NEXTJS_APP_PRIVATE_KEY],
+            chainId: 4,
+        },
+    },
+    etherscan: {
+        apiKey: process.env.NEXTJS_APP_ETHERSCAN_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
 };
--- scripts/deploySneakyBird.js
+++ scripts/deploySneakyBird.js
@@ -1,27 +1,23 @@
-const { network } = require("hardhat");
-const hre = require("hardhat");
-const { networkConfig } = require("../helper-hardhat-config");
-
+const hre = require('hardhat');
+const { networkConfig } = require('../helper-hardhat-config');
 
 async function main() {
+    // We get the contract to deploy
+    const SneakyBird = await hre.ethers.getContractFactory('SneakyBird');
+    const sneakyBird = await SneakyBird.deploy(
+        networkConfig[hre.network.config.chainId]['vrfCoordinator'],
+        networkConfig[hre.network.config.chainId]['linkToken'],
+        networkConfig[hre.network.config.chainId]['keyHash'],
+    );
 
-  // We get the contract to deploy
-  const SneakyBird = await hre.ethers.getContractFactory("SneakyBird");
-  const sneakyBird = await SneakyBird.deploy(
-    networkConfig[network.config.chainId]["vrfCoordinator"],
-    networkConfig[network.config.chainId]["linkToken"],
-    networkConfig[network.config.chainId]["keyHash"]
-  );
+    await sneakyBird.deployed();
 
-  await sneakyBird.deployed();
-
-  console.log("SneakyBird deployed to the", network.name, "network at:", sneakyBird.address);
+    console.log('SneakyBird deployed to the', hre.network.name, 'network at:', sneakyBird.address);
 }
 
-
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  }); 
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
