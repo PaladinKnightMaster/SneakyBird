5aa0e8234df0a5482e774986fea0efbcc5d6dbbd
PaladinKnightMaster <paladinknight.master@gmail.com>
2022-04-09 22:43:45 -0400

    Added Test to Fund Link to SneakyBird Contract

--- test/sneakyBird_test.js
+++ test/sneakyBird_test.js
@@ -1,26 +1,36 @@
 const { expect } = require('chai');
 const { ethers, network } = require('hardhat');
-const { networkConfig } = require('../helper-hardhat-config');
+const { networkConfig, getNetworkIdFromName } = require('../helper-hardhat-config');
 
 describe('SneakyBird', function () {
-    it('Should Deploy the SneakyBird Contract and pass the VRFCoordinator args to the constructor', async function () {
-        const SneakyBird = await ethers.getContractFactory('SneakyBird');
-        const sneakybird = await SneakyBird.deploy(
-            networkConfig[network.config.chainId]['vrfCoordinator'],
-            networkConfig[network.config.chainId]['linkToken'],
-            networkConfig[network.config.chainId]['keyHash'],
-        );
-        await sneakybird.deployed(
-            console.log(
-                '   Contract deployed to the',
-                network.name,
-                'network at:',
-                sneakybird.address,
-            ),
-        );
 
-        // Still worling on the contract... need to finish these tests:
-        // 1. Funding Link to the deployed Contract
-        // 2. Call CreateNFT function to try to mint an NFT.
-    });
-});
+
+    it('Should Deploy the SneakyBird Contract and fund it with LINK token', async function () {
+        const { get } = deployments;
+        const chainId = await getChainId();
+        const accounts = await hre.ethers.getSigners();
+        const [signer] = accounts;
+        const keyHash = networkConfig[chainId].keyHash;
+        let linkTokenAddress;
+        let vrfCoordinatorAddress;
+        if (chainId == 31337) {
+            const linkToken = await get('LinkToken');
+            const VRFCoordinatorMock = await get('VRFCoordinatorMock');
+            linkTokenAddress = linkToken.address;
+            vrfCoordinatorAddress = VRFCoordinatorMock.address;
+        } else {
+            linkTokenAddress = networkConfig[chainId].linkToken;
+            vrfCoordinatorAddress = networkConfig[chainId].vrfCoordinator;
+        }
+        // Deployment of SneakyBird Contract
+        const SneakyBird = await ethers.getContractFactory('SneakyBird');
+        sneakybird = await SneakyBird.deploy(vrfCoordinatorAddress, linkTokenAddress, keyHash);
+        await sneakybird.deployed(console.log('   Contract deployed to the', network.name, 'network at:', sneakybird.address,));
+        // fund the SneakyBird Contract with LINK
+        const fundAmount = String(0.1 * 10 ** 18);
+        const linkTokenContract = await ethers.getContractFactory('LinkToken');
+        const linkToken = new ethers.Contract(linkTokenAddress, linkTokenContract.interface, signer);
+        let fund_tx = await linkToken.transfer(sneakybird.address, fundAmount);
+        await fund_tx.wait(1);
+    })
+})
