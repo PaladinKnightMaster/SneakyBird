672f2e4dcf1c45848d66873b987b3930b74dee1b
PaladinKnightMaster <paladinknight.master@gmail.com>
2022-05-13 22:19:16 -0400

    modified solidity contract
    Modified Solidity contract, got rid of imports, a mapping, setTokenURIs statements that were redundant and added requires to create the NFT, based on quantity, and tracking the creation of gold nfts.

--- contracts/SneakyBird.sol
+++ contracts/SneakyBird.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.8.3;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
-// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import '@chainlink/contracts/src/v0.8/VRFConsumerBase.sol';
 
 contract SneakyBird is ERC721URIStorage, Ownable, VRFConsumerBase {
   mapping(bytes32 => address) public requestIdToSender;
-  mapping(bytes32 => string) public requestIdToTokenURI;
   mapping(uint256 => Material) public tokenIdToMaterial;
   mapping(bytes32 => uint256) public requestIdToTokenId;
+  mapping(address => uint256) public walletMints; // Track the quantity of mints on a wallet
   event requestedNFT(bytes32 indexed requestId); // for testing purposes only
 
   bytes32 internal keyHash; // Created Variables to interact with Chainlink VRF
   uint256 internal fee; // Created Variables to interact with Chainlink VRF
   uint256 public tokenCounter;
+  uint256 internal goldToken;
   address payable public withdrawWallet;
   uint256 public mintPrice;
   uint256 public maxSupply;
@@ -39,36 +39,45 @@ contract SneakyBird is ERC721URIStorage, Ownable, VRFConsumerBase {
     maxSupply = 925; // Not being used yet
     maxPerWallet = 3; // Not being used yet
     withdrawWallet = payable(msg.sender);
+    goldToken = 0;
   }
 
   // This next function is the main function to create an NFT, once the function is called then...
   // it makes a requestRandomness to Chainlink VRF and the response is a callback to the next function called fulfillRandomness.
 
-  function createNFT(string memory tokenURI) public returns (bytes32) {
-    bytes32 requestId = requestRandomness(keyHash, fee);
-    requestIdToSender[requestId] = msg.sender;
-    requestIdToTokenURI[requestId] = tokenURI;
-    emit requestedNFT(requestId); // for testing purposes only
-    return requestId;
+  function createNFT(string memory tokenURI, uint256 _quantity) public payable returns (bytes32) {
+    require(isPublicMintEnabled, 'Minting is not enabled yet');
+    require(msg.value == _quantity * mintPrice, 'Wrong mint Price');
+    require(tokenCounter + _quantity <= maxSupply, 'Collection is Sold Out');
+    require(walletMints[msg.sender] + _quantity <= maxPerWallet, 'wallet exceeded max of mints');
+
+    for (uint256 i = 0; i < _quantity; i++) {
+      bytes32 requestId = requestRandomness(keyHash, fee);
+      requestIdToSender[requestId] = msg.sender;
+      emit requestedNFT(requestId); // for testing purposes only
+      return requestId;
+    }
   }
 
   // Callback function used by the Chainlink VRF Contract to render a random number
 
   function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
     address mushroomOwner = requestIdToSender[requestId];
-    string memory tokenURI = requestIdToTokenURI[requestId];
     uint256 newItemId = tokenCounter;
     _safeMint(mushroomOwner, newItemId);
-    setTokenURI(newItemId, tokenURI);
     Material material = Material(randomNumber % 2);
-    // Here's where I need to create a counter for GOLD NFTs, we can only mint 6 Gold units for the 925 supply
-    // Or maybe create a separate MINT Function with logic included
-    tokenIdToMaterial[newItemId] = material;
+    if (material == Material(1) && goldToken < 6) {
+      tokenIdToMaterial[newItemId] = Material(1);
+      goldToken + 1;
+    } else {
+      tokenIdToMaterial[newItemId] = Material(0);
+    }
+    setTokenURI(newItemId, tokenURI);
     requestIdToTokenId[requestId] = newItemId;
     tokenCounter = tokenCounter + 1;
   }
 
-  function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+  function setTokenURI(uint256 tokenId, string _tokenURI) public {
     require(
       _isApprovedOrOwner(_msgSender(), tokenId),
       'ERC721: transfer caller is not owner nor approved'
